5628e6e0e956e399e73653087243a5241fe5fa7d
Ppaja <73185125+Ppaja@users.noreply.github.com>
2023-10-12 11:00:20 +0200

    lots of small things
    fixes of a few small things

--- theme.js
+++ theme.js
@@ -1,93 +1,53 @@
-// Theme 1
-
-function theme1() {
-  document.body.style.backgroundColor = "var(--theme1-main)";
-  document.body.style.color = "var(--theme1-text)";
-
-  var rootBackgroundColor = getComputedStyle(
-    document.documentElement
-  ).getPropertyValue("--theme1-button");
-
-  var rootColor = getComputedStyle(document.documentElement).getPropertyValue(
-    "--theme1-button"
-  );
+function applyTheme(mainBgColor, textColor, buttonBgColor, buttonTextColor) {
+  document.body.style.backgroundColor = mainBgColor;
+  document.body.style.color = textColor;
 
   var anchors = document.querySelectorAll("a");
   var buttons = document.querySelectorAll("button");
   var cards = document.querySelectorAll(".card");
 
   anchors.forEach(function (anchor) {
-    anchor.style.color = rootColor;
+    anchor.style.color = textColor;
   });
 
   buttons.forEach(function (button) {
-    button.style.backgroundColor = rootBackgroundColor;
+    button.style.backgroundColor = buttonBgColor;
+    button.style.color = buttonTextColor;
   });
 
   cards.forEach(function (card) {
-    card.style.backgroundColor = rootBackgroundColor;
+    card.style.backgroundColor = buttonBgColor;
   });
 }
 
-// Theme 2
-
-function theme2() {
-  document.body.style.backgroundColor = "var(--theme2-main)";
-  document.body.style.color = "var(--theme2-text)";
-
-  var rootBackgroundColor = getComputedStyle(
-    document.documentElement
-  ).getPropertyValue("--theme2-button");
-
-  var rootColor = getComputedStyle(document.documentElement).getPropertyValue(
-    "--theme2-button"
+// Theme 1
+function theme1() {
+  applyTheme(
+    "var(--theme1-main)",
+    "var(--theme1-text)",
+    "var(--theme1-button)",
+    "var(--theme1-main)"
   );
+}
 
-  var anchors = document.querySelectorAll("a");
-  var buttons = document.querySelectorAll("button");
-  var cards = document.querySelectorAll(".card");
-
-  anchors.forEach(function (anchor) {
-    anchor.style.color = rootColor;
-  });
-
-  buttons.forEach(function (button) {
-    button.style.backgroundColor = rootBackgroundColor;
-  });
-
-  cards.forEach(function (card) {
-    card.style.backgroundColor = rootBackgroundColor;
-  });
+// Theme 2
+function theme2() {
+  applyTheme(
+    "var(--theme2-main)",
+    "var(--theme2-text)",
+    "var(--theme2-button)",
+    "var(--theme2-main)"
+  );
 }
 
 // Theme 3
 function theme3() {
-  document.body.style.backgroundColor = "var(--theme3-main)";
-  document.body.style.color = "var(--theme3-text)";
-
-  var rootBackgroundColor = getComputedStyle(
-    document.documentElement
-  ).getPropertyValue("--theme3-button");
-
-  var rootColor = getComputedStyle(document.documentElement).getPropertyValue(
-    "--theme3-button"
+  applyTheme(
+    "var(--theme3-main)",
+    "var(--theme3-text)",
+    "var(--theme3-button)",
+    "var(--theme3-main)"
   );
-
-  var anchors = document.querySelectorAll("a");
-  var buttons = document.querySelectorAll("button");
-  var cards = document.querySelectorAll(".card");
-
-  anchors.forEach(function (anchor) {
-    anchor.style.color = rootColor;
-  });
-
-  buttons.forEach(function (button) {
-    button.style.backgroundColor = rootBackgroundColor;
-  });
-
-  cards.forEach(function (card) {
-    card.style.backgroundColor = rootBackgroundColor;
-  });
 }
 
 function loadTheme() {
